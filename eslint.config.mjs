5a5d96adbf7a533b5cd72006e1d911641c64a66b
olavodario <olavoribgodario@gmail.com>
2026-06-23 16:19:04 -0300

    chore(repo): configure typescript, eslint, prettier and husky

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+  {
+    ignores: ["**/node_modules/**", "**/dist/**", "**/.turbo/**"],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsparser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-require-imports": "error",
+    },
+  },
+];
